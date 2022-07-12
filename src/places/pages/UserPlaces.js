773853501cfac92459e48d996249bbed16d75c21
Yuhui Tong <tongyuhui4545@gmail.com>
2022-07-12 17:27:08 +0800

    user can now click on the user card and see the place list

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "UNIQLO STORE",
+    description: "Best Fast Fashion Apparel Chain Store",
+    imageUrl:
+      "https://prosple-content-api.global.ssl.fastly.net/sites/default/files/styles/scale_and_crop_center_890x320/public/2019-08/Banner-Uniqlo-Australia-890x320-2019.png?itok=DD1g-Hc-",
+    address: "UNIQLO Harbour City, 3231 Canton Rd, Tsim Sha Tsui",
+    location: { lat: 22.2812365, lng: 114.1809975 },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "EB Games",
+    description: "Game Store",
+    imageUrl:
+      "https://e.dam-img.rfdcontent.com/offers/008/841/553/600x600_smart_fit.jpg",
+    address: "27 Harbour Rd, Wan Chai",
+    location: { lat: 22.277152, lng: 114.1561324 },
+    creator: "u2",
+  },
+  {
+    id: "p3",
+    title: "Yarra Rivers",
+    description: "Melbourne River",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Melbourne_%28AU%29%2C_Yarra_River_--_2019_--_1431.jpg/1200px-Melbourne_%28AU%29%2C_Yarra_River_--_2019_--_1431.jpg",
+    address:
+      "10/F Lockhart Road Municipal Services Building, 225 Hennessy Road, Wanchai",
+    location: { lat: 22.281354, lng: 114.1675203 },
+    creator: "u3",
+  },
+  {
+    id: "p4",
+    title: "DFO",
+    description: "Cheap Factory Store",
+    imageUrl:
+      "https://www.shoppingcentrenews.com.au/wp-content/uploads/DFO-Uni-Hill-entry-IIII.jpg",
+    address:
+      "Wan Chai, Hennessy Rd, 423號, Ka Nin Wah Commercial Building, 16B",
+    location: { lat: 22.281354, lng: 114.1675203 },
+    creator: "u4",
+  },
+];
+
+function UserPlaces() {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+}
+
+export default UserPlaces;
--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,32 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <>
-    <SideDrawer>
-      <nav className="main-navigation__drawer-nav">
-        <NavLinks />
-      </nav>
-      </ SideDrawer>
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/"> YourPlaces</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks />
-      </nav>
-    </MainHeader>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      {drawerIsOpen ? (
+        <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      ) : null}
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/"> YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
     </>
   );
 };
