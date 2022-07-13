daf4c4375c43c0a447e61d8c1c42ef6f80157c0e
Yuhui Tong <tongyuhui4545@gmail.com>
2022-07-13 14:34:30 +0800

    [feat]
    Map function is added
    
    The API is from google map
    
    This closes the case DQ-3475

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
-import "./PlaceItem.css";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
+import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
@@ -23,7 +24,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>Close</Button>}
       >
         <div className="map-container">
-          <h2>The Map!</h2>
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
 
