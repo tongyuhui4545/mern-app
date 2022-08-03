db3e87a08401b0c3eedb3f23258a70506c5e04b9
tongyuhui4545 <tongyuhui4545@gmail.com>
2022-08-03 20:19:47 +0800

    Error Model
    Error Model is added.
    
    Now the Error handler looks cleaner

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -22,9 +24,7 @@ router.get("/:pid", (req, res, next) => {
     return p.id === placeId;
   });
   if (!place) {
-    const error = new Error("Could not find a place for the provided id");
-    error.code = 404;
-    throw error;
+    throw new HttpError("Could not find a place for this id", 404);
   }
   res.json({ place });
 });
@@ -35,9 +35,7 @@ router.get("/user/:uid", (req, res, next) => {
     return p.creator === userId;
   });
   if (!place) {
-    const error = new Error("Could not find a place for the provided id");
-    error.code = 404;
-    next(error);
+    return next(new HttpError("Could not find such a user", 404));
   }
   res.json({ place });
 });
