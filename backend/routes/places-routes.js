5afbc4487ff6de8d705fa68dd10b7866571f782a
tongyuhui4545 <tongyuhui4545@gmail.com>
2022-08-03 19:06:12 +0800

    basic routes added
    Basic routes added, backend dev starts

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,35 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("Get Request in Places");
-  res.json("This is a test");
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Uniqlo Store",
+    description: "One of the best international apparel chain stores",
+    location: {
+      lat: 37.8112497,
+      lng: 144.8140554,
+    },
+    address: "269/321 Lonsdale St, Melbourne VIC 3000, Australia",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  res.json({ place });
 });
 
 module.exports = router;
