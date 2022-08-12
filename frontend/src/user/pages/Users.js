3c729a56a176d933fa51c46b12c3a3f7d98d2ea9
tongyuhui4545 <tongyuhui4545@gmail.com>
2022-08-12 16:56:27 +0800

    Getting User with a GET Request
    Now the users can be created and fetched to the user-list page

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-import UsersList from '../components/UsersList'
-
-const USERS = [{id:"u4", name: "tyh", image: "https://www.thespruceeats.com/thmb/E1v3koUrFM5HhL6Yj8HKNuSmLoY=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/white-and-red-sauce-lasagna-recipe-995925-hero-1-fb2c2142b39042069f0c50310047256d.jpg", places: 4}];
+import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const Users = () => {
-  return (
-    <UsersList items={USERS} />
-  )
-}
+  const [isLoading, setIsLoading] = useState();
+  const [error, setError] = useState();
+  const [loadedUsers, setLoadedUsers] = useState();
 
-export default Users
+  useEffect(() => {
+    const sendRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
+  }, []);
+
+  const errorHandler = () => {};
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
+};
+
+export default Users;
